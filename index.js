d0ce64121699c96665fe6e490a58cace10d2a44a
cofeek-codes <kormysev11@mail.ru>
2023-06-22 09:30:22 +0500

    modal

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const price = document.querySelector('.sellmain')
 
-const submitButton = document.querySelector('#buy')
+const submitButton = document.querySelector('.buy')
+const date = document.querySelector('#date').value
 
 const initialPriceValue = 5000
 const htmlBase = 'Итоговая стоймость:'
@@ -8,12 +9,10 @@ const htmlBase = 'Итоговая стоймость:'
 const formatPrice = price => {
 	return htmlBase + ' ' + (initialPriceValue + price)
 }
-
-submitButton.addEventListener('click', () => {
-	const date = document.querySelector('#date').value
-	const city = document.querySelector('#selector').value
-
-	switch (city) {
+const city = document.querySelector('#selector')
+price.innerHTML = formatPrice(5000)
+city.addEventListener('change', () => {
+	switch (city.value) {
 		case 'Москва':
 			price.innerHTML = formatPrice(5000)
 			break
@@ -28,3 +27,31 @@ submitButton.addEventListener('click', () => {
 			break
 	}
 })
+
+// modal
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+// modal
